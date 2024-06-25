1adfa78f803cda36af4b7606aa1645773622a9be
achoqueo <achoqueo@unjbg.edu.pe>
2024-06-24 20:30:40 -0500

    Opciones iniciales de la calculadora

---
+++ ejercicio1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    float num1, num2;
+    float r;
+
+    cout << "Ingrese el primer numero: ";cin>>num1;
+    cout << "Ingrese el segundo numero: ";cin>>num2;
+    cout << "Ingrese un operador: "; cin >> op;
+
+    switch(op) {
+        case '+':
+        	r=num1+num2;
+            cout <<"La suma es "<<r<<endl;
+            system("pause");
+            break;
+        case '-':
+        	r=num1-num2;
+            cout <<"La resta es "<<r<<endl;
+            system("pause");
+            break;
+        case '*':
+        	r=num1*num2;
+            cout <<"La multiplicacion es "<<r<<endl;
+            system("pause");
+            break;
+        case '/':
+            r=num1/num2;
+            cout<<"La division es "<<r<<endl;
+            system("pause");
+            break;
+    }
+
+    return 0;
+}
+
